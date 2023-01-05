b002d3984040a35d157c9e5cffb3f4fe09cee156
hannasoderstrom1 <hanna.soderstrom@gu.se>
2023-01-05 08:25:10 +0100

    initial template build from hydra-genetics/tools, version 0.15.0

---
+++ workflow/scripts/dummy.py
@@ -0,0 +1,11 @@
+# vim: syntax=python tabstop=4 expandtab
+# coding: utf-8
+
+__author__ = "Hanna Soderstrom"
+__copyright__ = "Copyright 2023, Hanna Soderstrom"
+__email__ = "hanna.soderstrom@gu.se"
+__license__ = "GPL-3"
+
+
+def dummy():
+    return 1
