833d26a63155ecb2572fc7fd2623bab154e3db88
hannasoderstrom1 <hanna.soderstrom@gu.se>
2023-04-13 14:13:10 +0200

    restore dummy scripts

---
+++ workflow/scripts/test_dummy.py
@@ -0,0 +1,9 @@
+__author__ = "Hanna Soderstrom"
+__copyright__ = "Copyright 2023, Hanna Soderstrom"
+__email__ = "hanna.soderstrom@gu.se"
+__license__ = "GPL-3"
+
+
+def test_dummy():
+    from dummy import dummy
+    assert dummy() == 1
